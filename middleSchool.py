072d9854fcd9f7357ec057c688dcecfc6d6d674f
Richard Zhu <richardzhu@Richards-MBP.hsd1.ca.comcast.net>
2021-08-06 01:22:03 -0700

    Calculate Pearson's coefficient for applications and admissions, cleaned dataset

--- middleSchool.py
+++ middleSchool.py
@@ -7,7 +7,18 @@ Created on Mon Aug  2 16:32:56 2021
 """
 
 import numpy as np
-from scipy import stats
-import matplotlib.pyplot as plt
 
-data = np.genfromtxt('middleSchoolData.csv', delimiter = ',', skip_header = 1)
+# Remove commas from school names
+with open("middleSchoolData.csv") as infile, open("fixedData.csv", "w") as outfile:
+    for line in infile:
+        outfile.write(line.replace(", ", " "))
+    
+# Retrieve clean data
+data = np.genfromtxt('fixedData.csv', delimiter=',', skip_header = 1, dtype='unicode')
+
+# Correlation between application and admission
+application = data[:,2].astype(np.int)
+admission = data[:,3].astype(np.int)
+corrArr = np.corrcoef(application, admission)
+corr = corrArr[0, 1]
+
