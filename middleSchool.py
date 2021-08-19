87bc8f196cedef062ad00459bf617d6086d7acc0
Richard Zhu <richardzhu78@gmail.com>
2021-08-19 16:27:06 -0400

    Clean up code and fix PCAs

--- middleSchool.py
+++ middleSchool.py
@@ -8,8 +8,8 @@ Created on Mon Aug  2 16:32:56 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import linear_model 
 import scipy.stats as st
+from sklearn.decomposition import PCA
 
 # Remove commas from school names
 with open("middleSchoolData.csv") as infile, open("fixedData.csv", "w") as outfile:
@@ -51,6 +51,13 @@ appRate = appMod/schoolSize
 corrArr = np.corrcoef(appRate, admMod)
 corr = corrArr[0, 1]
 
+plt.figure(1)
+plt.title("Correlation Between Application Rate and Acceptances to HSPHS")
+plt.xlabel("Application Rate")
+plt.ylabel("Acceptances")
+plt.plot(appRate, admMod, 'o', color='black', markersize=2)
+plt.plot(np.unique(appRate), np.poly1d(np.polyfit(appRate, admMod, 1))(np.unique(appRate)))
+
 # Calculating per student odds
 bestOdds = 0
 index = 0
@@ -64,85 +71,82 @@ for ii in range(len(delete)):
     ind = len(delete) - 1 - ii
     dbn = np.delete(dbn, delete[ind])
 
-plt.figure(1)
+plt.figure(2)
 plt.title("Per Student Odds of Admission to HSPHS")
 plt.xlabel("School Size")
 plt.ylabel("Acceptances")
 plt.plot(schoolSize, admMod, 'o', color='black', markersize=2)
 
-#Multiple regression
-X = np.transpose([data[:,11],data[:,12],data[:,13],data[:,14],data[:,15],data[:,16]])
-
-A = data[:,21] # student achievement
-R = data[:,22] # reading exceedd
-M = data[:,23] # math exceed
+#PCA and linear regression
+perception = np.transpose([data[:,11],data[:,12],data[:,13],data[:,14],data[:,15],data[:,16]])
+achievement =  np.transpose([data[:,21],data[:,22],data[:,23]])
 
 delete = []
-for ii in range(len(X)):
-    for jj in range(len(X[0])):
-        if X[ii,jj] == "":
+for ii in range(len(perception)):
+    for jj in range(len(perception[0])):
+        if perception[ii,jj] == "":
             if ii not in delete: delete.append(ii)
-    if A[ii] == "" or R[ii] == "" or M[ii] == "":
-        if ii not in delete: delete.append(ii)
+    for jj in range(len(achievement[0])):
+        if achievement[ii,jj] == "":
+            if ii not in delete: delete.append(ii)
 for ii in range(len(delete)):
     index = len(delete) - 1 - ii
-    X = np.delete(X, delete[index], axis=0)
-    A = np.delete(A, delete[index])
-    R = np.delete(R, delete[index])
-    M = np.delete(M, delete[index])
+    perception = np.delete(perception, delete[index], axis=0)
+    achievement = np.delete(achievement, delete[index], axis=0)
     
-X = X.astype(float)
-A = A.astype(float)
-R = R.astype(float)
-M = M.astype(float)
-
-regr = linear_model.LinearRegression() 
-regr.fit(X,A)
-rSqrA = regr.score(X,A)
-regr.fit(X,R)
-rSqrR = regr.score(X,R)
-regr.fit(X,M)
-rSqrM = regr.score(X,M)
-
-plt.figure(2)
-fig, axs = plt.subplots(2, 3)
-fig.suptitle('Relationships Between Student Perception and Student Achievement')
-axs[0, 0].plot(X[:,0], A, 'o', markersize=0.75)
-axs[0, 0].set_title('Rigorous instruction', fontsize=7)
-axs[0, 1].plot(X[:,1], A, 'o', color='orange', markersize=0.75)
-axs[0, 1].set_title('Collaborative teachers', fontsize=7)
-axs[0, 2].plot(X[:,2], A, 'o', color='green', markersize=0.75)
-axs[0, 2].set_title('Supportive environment', fontsize=7)
-axs[1, 0].plot(X[:,3], A, 'o', color='red', markersize=0.75)
-axs[1, 0].set_title('Effictive school leadership', fontsize=7)
-axs[1, 1].plot(X[:,4], A, 'o', color='purple', markersize=0.75)
-axs[1, 1].set_title('Strong family community ties', fontsize=7)
-axs[1, 2].plot(X[:,5], A, 'o', color='black', markersize=0.75)
-axs[1, 2].set_title('Trust', fontsize=7)
-for ii in range(2):
-    for jj in range(3):
-        axs[ii, jj].get_xaxis().set_visible(False)
-        axs[ii, jj].get_yaxis().set_visible(False)
+perception = perception.astype(float)
+achievement = achievement.astype(float)
 
 plt.figure(3)
-fig, axs = plt.subplots(2, 3)
-fig.suptitle('Relationships Between Student Perception and Reading Scores')
-axs[0, 0].plot(X[:,0], R, 'o', markersize=0.75)
-axs[0, 0].set_title('Rigorous instruction', fontsize=7)
-axs[0, 1].plot(X[:,1], R, 'o', color='orange', markersize=0.75)
-axs[0, 1].set_title('Collaborative teachers', fontsize=7)
-axs[0, 2].plot(X[:,2], R, 'o', color='green', markersize=0.75)
-axs[0, 2].set_title('Supportive environment', fontsize=7)
-axs[1, 0].plot(X[:,3], R, 'o', color='red', markersize=0.75)
-axs[1, 0].set_title('Effictive school leadership', fontsize=7)
-axs[1, 1].plot(X[:,4], R, 'o', color='purple', markersize=0.75)
-axs[1, 1].set_title('Strong family community ties', fontsize=7)
-axs[1, 2].plot(X[:,5], R, 'o', color='black', markersize=0.75)
-axs[1, 2].set_title('Trust', fontsize=7)
-for ii in range(2):
-    for jj in range(3):
-        axs[ii, jj].get_xaxis().set_visible(False)
-        axs[ii, jj].get_yaxis().set_visible(False)
+rP = np.corrcoef(perception,rowvar=False)
+plt.imshow(rP) 
+plt.colorbar()
+
+plt.figure(4)
+rA = np.corrcoef(achievement,rowvar=False)
+plt.imshow(rA) 
+plt.colorbar()
+
+zscoredDataP = st.zscore(perception)
+pcaP = PCA().fit(zscoredDataP)
+eigValsP = pcaP.explained_variance_
+loadingsP = pcaP.components_
+newP = PCA().fit_transform(zscoredDataP)
+numPerceptions = 6
+
+plt.figure(5)
+plt.xlabel('Principal component')
+plt.ylabel('Eigenvalue')
+plt.bar(np.linspace(1,6,6),eigValsP)
+plt.plot([0,numPerceptions],[1,1],color='red',linewidth=1) 
+
+plt.figure(6)
+plt.xlabel('Student Perception')
+plt.ylabel('Loading')
+plt.bar(np.linspace(1,6,6),loadingsP[0,:]*-1)
+
+zscoredDataA = st.zscore(achievement)
+pcaA = PCA().fit(zscoredDataA)
+eigValsA = pcaA.explained_variance_
+loadingsA = pcaA.components_
+newA = PCA().fit_transform(zscoredDataA)
+numMeasurements = 3
+
+plt.figure(7)
+plt.xlabel('Principal component')
+plt.ylabel('Eigenvalue')
+plt.bar(np.linspace(1,3,3),eigValsA)
+plt.plot([0,numMeasurements],[1,1],color='red',linewidth=1) 
+
+plt.figure(8)
+plt.xlabel('Objective Achievement')
+plt.ylabel('Loading')
+plt.bar(np.linspace(1,3,3),loadingsA[0,:]*-1)
+
+perceptionComponent = newP[:,0]
+achievementComponent = newA[:,0]
+slope, intercept, r_value, p_value, std_err = st.linregress(perceptionComponent, achievementComponent)
+cod = r_value**2
 
 # Small vs large school
 schoolSizeMod = schoolSize[appMod>0]
@@ -154,38 +158,127 @@ smallSchools = admRate[schoolSizeMod < median]
 largeSchools = admRate[schoolSizeMod >= median]
 pValSL = st.ttest_ind(smallSchools, largeSchools)
 
+plt.title("Correlation Between School Size and Acceptance Rate to HSPHS")
+plt.xlabel("School Size")
+plt.ylabel("Acceptance Rate")
+plt.scatter(schoolSizeMod[schoolSizeMod < median], smallSchools, s=1.5, c='b', marker="s", label='Small Schools')
+plt.scatter(schoolSizeMod[schoolSizeMod >= median], largeSchools, s=1.5, c='r', marker="s", label='Large Schools')
+plt.legend(loc='upper right');
+plt.show()
+
 #Per student spending vs student achievemnt
 spending = data[:,4]
-achievement = data[:,21]
-reading = data[:,22]
+obj = np.transpose([data[:,21],data[:,22],data[:,23]])
 delete = []
 for ii in range(len(spending)):
-    if spending[ii] == "" or achievement[ii] == "" or reading[ii] == "":
+    if spending[ii] == "":
         if ii not in delete: delete.append(ii)
+    for jj in range(len(obj[0])):
+        if obj[ii,jj] == "":
+            if ii not in delete: delete.append(ii)
 for ii in range(len(delete)):
     index = len(delete) - 1 - ii
     spending = np.delete(spending, delete[index])
-    achievement = np.delete(achievement, delete[index])
-    reading = np.delete(reading, delete[index])
+    obj = np.delete(obj, delete[index], axis=0)
 spending = spending.astype(int)
-achievement = achievement.astype(float)
-reading = reading.astype(float)
-plt.figure(5)
-plt.title("Correlation Between Per Student Spending and Student Achievement")
+obj = obj.astype(float)
+
+zs = st.zscore(obj)
+pcaObj = PCA().fit(zs)
+eigValues = pcaObj.explained_variance_ 
+loadings = pcaObj.components_ *-1
+newObj = pcaObj.fit_transform(zs)*-1                    
+
+achievement = newObj[:,0]
+
+plt.figure(9)
+plt.title("Correlation Between Per Student Spending and Objective Student Achievement")
 plt.xlabel("Per Student Spending")
-plt.ylabel("Student Achievement")
+plt.ylabel("Objective Student Achievement")
 plt.plot(spending, achievement, 'o', color='black', markersize=2)
 medianSpending = np.median(spending)
 lowSpending = achievement[spending < medianSpending]
 highSpending = achievement[spending >= medianSpending]
-pValLHA = st.ttest_ind(lowSpending, highSpending) 
-plt.figure(6)
-plt.title("Correlation Between Per Student Spending and Reading Scores Exceed")
-plt.xlabel("Per Student Spending")
-plt.ylabel("Reading Scores Exceed")
-plt.plot(spending, reading, 'o', color='black', markersize=2)
-medianSpending = np.median(spending)
-lowSpending = reading[spending < medianSpending]
-highSpending = reading[spending >= medianSpending]
-pValLHR = st.ttest_ind(lowSpending, highSpending) 
+pValObj = st.ttest_ind(lowSpending, highSpending) 
+
+# Proportion of schools
+totalAcceptances = np.sum(admission)
+sortedAcceptances = np.sort(admission)[::-1] #sort in descending order
+total = 0
+counter = 0
+
+for ii in sortedAcceptances:
+    total += ii
+    counter += 1
+    if total >= (totalAcceptances*0.9):
+        break   
+proportion = counter/len(admission)
+
+plt.figure(11)
+plt.title("Number of Acceptances Sorted in Decreasing Order")
+plt.xlabel("Schools in Decreasing Order")
+plt.ylabel("Number of Acceptances")
+plt.bar(np.linspace(1,200,200),sortedAcceptances[0:200])
+plt.axvline(x=123, color="red")
+
+#Prediction
+predictors = np.transpose([data[:,2],data[:,4],data[:,5],data[:,6],data[:,7],data[:,8],
+                           data[:,9],data[:,10],data[:,11],data[:,12],data[:,13],data[:,14],
+                           data[:,15],data[:,16],data[:,17],data[:,18],data[:,19],data[:,20]])
+adm = admission
+ach = np.transpose([data[:,21],data[:,22],data[:,23]])
+
+delete = []
+for ii in range(len(predictors)):
+    for jj in range(len(predictors[0])):
+        if predictors[ii,jj] == "" or adm[ii] == "":
+            if ii not in delete: delete.append(ii)
+    for jj in range(len(ach[0])):
+        if ach[ii,jj] == "":
+            if ii not in delete: delete.append(ii)
+for ii in range(len(delete)):
+    index = len(delete) - 1 - ii
+    predictors = np.delete(predictors, delete[index], axis=0)
+    adm = np.delete(adm, delete[index])
+    ach = np.delete(ach, delete[index], axis=0)
     
+predictors = predictors.astype(float)
+ach = ach.astype(float)
+    
+zscoredData = st.zscore(predictors)
+pca = PCA().fit(zscoredData)
+eigValues = pca.explained_variance_ 
+loadings = pca.components_ *-1
+origDataNewCoordinates = pca.fit_transform(zscoredData)*-1
+
+plt.figure(12)
+plt.title('Scree plot')
+plt.xlabel('Principal Components')
+plt.ylabel('Eigenvalues')
+plt.bar(np.linspace(1,len(predictors[0]),len(predictors[0])),eigValues)
+plt.plot([0,len(predictors[0])],[1,1],color='red',linewidth=1)
+
+plt.figure(13)
+plt.xlabel('Predictors')
+plt.ylabel('Loading')
+plt.bar(np.linspace(1,len(predictors[0]),len(predictors[0])),loadings[0,:])                   
+plt.bar(np.linspace(1,len(predictors[0]),len(predictors[0])),loadings[1,:])
+
+schoolDistrictConditions = origDataNewCoordinates[:,0]
+relationalConditions = origDataNewCoordinates[:,1]
+slope, intercept, r_value, p_value, std_err = st.linregress(schoolDistrictConditions, adm)
+codSD = r_value**2
+slope, intercept, r_value, p_value, std_err = st.linregress(relationalConditions, adm)
+codRC = r_value**2    
+
+zscoredAch = st.zscore(ach)
+pcaAch = PCA().fit(zscoredAch)
+eigValues = pcaAch.explained_variance_ 
+loadings = pcaAch.components_ *-1
+newAch = pcaAch.fit_transform(zscoredAch)*-1                    
+
+objectiveAchievement = newAch[:,0]
+slope, intercept, r_value, p_value, std_err = st.linregress(schoolDistrictConditions, objectiveAchievement)
+codSDOA = r_value**2
+slope, intercept, r_value, p_value, std_err = st.linregress(relationalConditions, objectiveAchievement)
+codRCOA = r_value**2    
